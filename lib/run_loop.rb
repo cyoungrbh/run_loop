3b8b87bedb60b931ca31f6fd962d787415de539f
Joshua Moody <joshuajmoody@gmail.com>
2016-02-11 16:20:33 +0100

    DNSSD: detect bonjour services

--- lib/run_loop.rb
+++ lib/run_loop.rb
@@ -42,6 +42,7 @@ require "run_loop/http/error"
 require "run_loop/http/server"
 require "run_loop/http/request"
 require "run_loop/http/retriable_client"
+require "run_loop/dnssd"
 
 module RunLoop
 
